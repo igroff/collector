d48870bf969a892d4e837984264ff67c35869d1d
Ian Groff <ian@iangroff.com>
2015-04-20 19:25:08 -0500

    stuffs

--- bin/config.py
+++ bin/config.py
@@ -1,9 +1,10 @@
 import os
 import logging
 
-search_table_name = os.environ.get("SEARCH_DDB_TABLE_NAME", "collector_searches_3")
-user_table_name = os.environ.get("USER_DDB_TABLE_NAME", "")
 logging.basicConfig()
 log = logging.getLogger()
 log.setLevel(os.environ.get("LOG_LEVEL", "INFO"))
+
+search_table_name = os.environ.get("SEARCH_DDB_TABLE_NAME", "collector_searches_3")
+user_table_name = os.environ.get("USER_DDB_TABLE_NAME", "collector_users")
 
