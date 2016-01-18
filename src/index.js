9f6a63ff3dfb5b714b7cea6174622e21140eb078
igroff <ian@iangroff.com>
2016-01-18 12:42:11 -0600

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 // main.js
 var React = require('react');
 var ReactDOM = require('react-dom');
+var log = require('simplog');
+
+global.debug = true;
+
+var SearchButton = React.createClass({
+  startSearch: function(e){
+    log.debug("starting search");
+  },
+  render: function(props){
+    return (
+     <div>
+       <input type="button" onClick={this.startSearch} value="Start Input Search"/>
+       <button onClick={this.startSearch}>Start Search</button> 
+     </div>
+    );
+  }
+});
 
 ReactDOM.render(
-  <h1>Hello, world! And Stuff. And other Things.</h1>,
+  <div>
+    <h1>Hello, world! And Stuff. And other Things.</h1>
+    <SearchButton/>
+  </div>,
   document.getElementById('example')
 );
