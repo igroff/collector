18bfe8bce1ecd510c5ddcf57a4d6d5bf507e9efe
igroff <ian@iangroff.com>
2016-01-18 21:25:38 -0600

    WIP

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var log = require('simplog');
 var persist = require('./persistence.js');
+var _ = require('lodash');
 
 global.debug = true;
 var gameState = persist.loadGameData();
 
-var SearchButton = React.createClass({
+var StartSearchButton = React.createClass({
   startSearch: function(e){
     log.debug("starting search");
-    this.props.state.activeSearches.push({"new":"search"});
+    this.props.state.activeSearches.push({started: new Date().getTime()});
     persist.saveGameData(gameState);
   },
   render: function(props){
@@ -20,10 +21,50 @@ var SearchButton = React.createClass({
   }
 });
 
+var SearchInfo = React.createClass({
+  render: function(){
+    return (
+      <span>Started At: {this.props.search.started}</span>
+    );
+  }
+});
+
+var SearchInfoList = React.createClass({
+  getActiveSearches: function(){
+    return gameState.activeSearches;
+  },
+  getInitialState: function(){ return {searches: []}; },
+  componentDidMount: function(){ this.setState({searches: this.getActiveSearches()}); },
+  render: function(){
+    var searchInfos = _.map(this.state.searches, function(search){
+      return(
+        <li><SearchInfo search={search}/></li>
+      );
+    });
+    return (
+      <div>
+        <ul>
+          {searchInfos}
+        </ul>
+      </div>
+    );
+  }
+});
+
+var ClearSaveButton = React.createClass({
+  clearSave: function(e){ 
+    persist.deleteGameData();
+  },
+  render: function(props){
+    return ( <button onClick={this.clearSave}>Clear Save</button> );
+  }
+});
+
 ReactDOM.render(
     <div>
-      <h1>Howdy Bitches!</h1>
-      <SearchButton state={gameState}/>
+      <SearchInfoList searches={gameState.activeSearches}/>
+      <StartSearchButton state={gameState}/>
+      <p><ClearSaveButton/></p>
     </div>
   , document.getElementById('example')
 );
